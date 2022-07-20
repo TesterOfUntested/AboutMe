8a1918e68ab70451a71e85d02e9ae037aba7e654
Krzysztof Bigos <sprawdzany96@gmail.com>
2022-07-20 19:38:46 +0200

    Updated scroll positions

--- script/menu-scroll-updater.js
+++ script/menu-scroll-updater.js
@@ -10,32 +10,32 @@ $(window).scroll(function() {
   var scroll = $(window).scrollTop();
   var a = document.getElementsByTagName('a');
 
-  if (scroll >= 0 && scroll < 420) {
+  if (scroll >= 0 && scroll < 700) {
     removeAllActives();
     a[1].parentElement.classList.add('active');
   }
 
-  if (scroll >= 420 && scroll < 840) {
+  if (scroll >= 700 && scroll < 1100) {
     removeAllActives();
     a[2].parentElement.classList.add('active');
   }
 
-  if(scroll >= 840 && scroll < 1000) {
+  if(scroll >= 1100 && scroll < 1300) {
     removeAllActives();
     a[3].parentElement.classList.add('active');
   }
 
-  if(scroll >= 1000 && scroll < 1300) {
+  if(scroll >= 1300 && scroll < 1600) {
     removeAllActives();
     a[4].parentElement.classList.add('active');
   }
 
-  if(scroll >= 1300 && scroll < 1750) {
+  if(scroll >= 1600 && scroll < 1950) {
     removeAllActives();
     a[5].parentElement.classList.add('active');
   }
 
-  if(scroll >= 1750) {
+  if(scroll >= 1950) {
     removeAllActives();
     a[6].parentElement.classList.add('active');
   }
